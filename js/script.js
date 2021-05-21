b39cca83ad037aa0a2b8d1284480d3bc83fc0d00
MaximPVladimirovich <maxim.bleeker@gmail.com>
2021-05-21 11:54:19 -0500

    Render data to dom

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
-alert(`tada`);
+/*----- constants -----*/
+let spaceXLaunches;
+const BASE_URL = `https://api.spacexdata.com/v3/launches`
 
-console.log($)
+
+/*----- app's state (variables) -----*/
+
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
+$.ajax(`https://api.spacexdata.com/v3/launches?limit=9&order=desc`).then(function (data) {
+    spaceXLaunches = data;
+    renderData(spaceXLaunches)
+    console.log(spaceXLaunches)
+
+})
+
+function renderData(data) {
+    let html = spaceXLaunches.map(function (launch) {
+        return `
+        <article>
+        <h2>${launch.mission_name}</h2>
+        <p>${launch.launch_year}</p>
+        </article>`
+
+    })
+    $(`main`).html(html)
+
+}
